b1398a10fbfca1ccb3c608a45efdef03f8dec1fa
xianweihuihuan <2907404984@qq.com>
2024-11-08 03:05:07 +0800

    高精度乘法

--- mul.c
+++ mul.c
@@ -46,41 +46,43 @@ int main()
     int bb[1000] = {0};
     for (int i = 0; i < sza; i++)
     {
-        aa[sza - i - 1] = a[i] - '0';
+        aa[sza - i] = a[i] - '0';
     }
     for (int i = 0; i < szb; i++)
     {
-        bb[szb - i - 1] = b[i] - '0';
+        bb[szb - i] = b[i] - '0';
     }
     int cc[1100] = {0};
     int szc = sza + szb;
-    for (int i = 0; i < sza; i++)
+    for (int i = 1; i <= sza; i++)
     {
-        for (int j = 0; j < szb; j++)
+        for (int j = 1; j <= szb; j++)
         {
-            cc[i + j] += aa[i] * bb[j];
-            cc[i + j + 1] = cc[i + j] / 10;
-            cc[i + j] %= 10;
+            cc[i + j - 1] += aa[i] * bb[j];
+            cc[i + j] += cc[i + j - 1] / 10;
+            cc[i + j - 1] %= 10;
         }
     }
-    while (cc[szc - 1] == 0 && szc - 1 > 0)
+    while (cc[szc] == 0 && szc > 0)
     {
         szc--;
     }
     /*char ccc[1000];
-    for(int i = 0;i<szc;i++){
-        ccc[i] = cc[szc-i-1]+'0';
+    for (int i = 0; i < szc; i++)
+    {
+        ccc[i] = cc[szc - i] + '0';
     }
     ccc[szc] = '\0';
-    if(sp==1){
-        printf("-");
-    }
-    printf("%s\n",ccc);*/
     if (sp == 1)
     {
         printf("-");
     }
-    for (int i = szc - 1; i >= 0; i--)
+    printf("%s\n", ccc);*/
+    if (sp == 1)
+    {
+        printf("-");
+    }
+    for (int i = szc; i > 0; i--)
     {
         printf("%d", cc[i]);
     }
